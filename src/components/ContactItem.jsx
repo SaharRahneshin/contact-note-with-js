8378598863db83ae2caa9a21d6f0ca455305bf20
Sahar Rahneshin <sahar.rahneshin@gmail.com>
2024-01-23 19:13:47 +0330

    first commit

---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,17 @@
+import styles from "./ContactItem.module.css";
+function ContactItem({
+  data: {id, name, lastName, email, phone},
+  deleteHandler,
+}) {
+
+  return (
+    <li className={styles.item}>
+    <p><span>👤</span>{name} {lastName}</p>
+    <p><span>📭</span>{email}</p>
+    <p><span>☎</span>{phone}</p>
+    <button onClick={() => deleteHandler(id)}>🗑</button>
+    </li>
+  )
+}
+
+export default ContactItem;
